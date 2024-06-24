1382841aab2277633cbbd36de49aebcc2079f851
Baron <bao.huynh@enclave.vn>
2024-06-24 13:40:46 +0700

    style shadow for marker using canvas

--- app/src/main/java/com/example/kotlinandroidexample/MapsActivity.kt
+++ app/src/main/java/com/example/kotlinandroidexample/MapsActivity.kt
@@ -2,29 +2,31 @@ package com.example.kotlinandroidexample
 
 import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.graphics.BlurMaskFilter
+import android.graphics.BlurMaskFilter.Blur
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Matrix
+import android.graphics.Matrix.ScaleToFit
 import android.graphics.Paint
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffXfermode
 import android.graphics.RectF
-import android.graphics.drawable.Drawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract.Colors
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-
+import androidx.core.graphics.toColorInt
+import com.example.kotlinandroidexample.databinding.ActivityMapsBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.example.kotlinandroidexample.databinding.ActivityMapsBinding
 import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.TileOverlayOptions
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -55,6 +57,16 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+//        //Random 100 marker
+//        for (i in 0..500) {
+//            val lat = -90 + (180 * Math.random())
+//            val lon = -180 + (360 * Math.random())
+//            val latLng = LatLng(lat, lon)
+//            val markerOption = MarkerOptions().position(latLng).title("Marker in $lat, $lon")
+//            markerOption.icon(setIcon(this, R.drawable.anchor_gg_marker))
+//            mMap.addMarker(markerOption)
+//        }
+
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         val markerOption = MarkerOptions().position(sydney).title("Marker in Sydney")
@@ -66,17 +78,63 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     private fun setIcon(context: Context, drawableId: Int): BitmapDescriptor {
-        val drawable = ActivityCompat.getDrawable(context,drawableId)
-        val bitmap = Bitmap.createBitmap(drawable!!.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        val drawable = ActivityCompat.getDrawable(context, drawableId)
+        val bitmap = Bitmap.createBitmap(
+            drawable!!.intrinsicWidth,
+            drawable.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+        )
         val canvas = Canvas(bitmap)
 
         drawable.setBounds(0, 0, canvas.width, canvas.height)
         drawable.draw(canvas)
-
-
-
-        return BitmapDescriptorFactory.fromBitmap(bitmap)
+        val shadow =
+            addShadow(
+                bitmap,
+                drawable.intrinsicHeight,
+                drawable.intrinsicWidth,
+                0xFF707070.toInt(),
+                10,
+                0f,
+                15f
+            );
+        return BitmapDescriptorFactory.fromBitmap(shadow)
     }
 
+    private fun addShadow(
+        bm: Bitmap,
+        dstHeight: Int,
+        dstWidth: Int,
+        color: Int,
+        size: Int,
+        dx: Float,
+        dy: Float
+    ): Bitmap {
+        val mask = Bitmap.createBitmap(dstWidth, dstHeight, Bitmap.Config.ALPHA_8)
 
+        val scaleToFit = Matrix()
+        val src = RectF(0f, 0f, bm.width.toFloat(), bm.height.toFloat())
+        val dst = RectF(0f, 0f, dstWidth - 2 * dx, dstHeight - 2 * dy)
+        scaleToFit.setRectToRect(src, dst, ScaleToFit.CENTER)
+
+        val dropShadow = Matrix(scaleToFit)
+        dropShadow.postTranslate(dx, dy)
+
+        val maskCanvas = Canvas(mask)
+        val paint = Paint()
+        maskCanvas.drawBitmap(bm, dropShadow, paint)
+
+        val filter = BlurMaskFilter(size.toFloat(), Blur.NORMAL)
+        paint.reset()
+        paint.color = color
+        paint.setMaskFilter(filter)
+        paint.isFilterBitmap = true
+
+        val ret = Bitmap.createBitmap(dstWidth, dstHeight, Bitmap.Config.ARGB_8888)
+        val retCanvas = Canvas(ret)
+        retCanvas.drawBitmap(mask, 0f, 0f, paint)
+        retCanvas.drawBitmap(bm, scaleToFit, null)
+        mask.recycle()
+        return ret
+    }
 }
