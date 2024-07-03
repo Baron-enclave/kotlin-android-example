3c0c18dc7af80580c073a74ba2535122b07826fd
Baron <bao.huynh@enclave.vn>
2024-07-03 14:11:41 +0700

    implement bottom sheet

--- app/src/main/java/com/example/kotlinandroidexample/views/explore/ExploreFragment.kt
+++ app/src/main/java/com/example/kotlinandroidexample/views/explore/ExploreFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.kotlinandroidexample.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class ExploreFragment : Fragment() {
     override fun onCreateView(
@@ -15,9 +16,13 @@ class ExploreFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_explore, container, false)
+        BottomSheetBehavior.from(view.findViewById(R.id.bottom_sheet)).apply {
+            state = BottomSheetBehavior.STATE_HIDDEN
+            halfExpandedRatio = 0.6f
+            hideFriction = 0.3f
+            isHideable = true
+        }
         return view
-
-
     }
 
 }
